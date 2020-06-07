7ea369d0ef96978f79514a045d2acf303925e2f6
Nestor hdez <nestorhdez.26@gmail.com>
2020-06-07 17:36:40 +0100

    Simplify index file and remove node-fetch

--- src/function/index.ts
+++ src/function/index.ts
@@ -1,40 +1,21 @@
 import {APIGatewayEvent, Context} from 'aws-lambda';
-import fetch from 'node-fetch';
-
-interface Todo {
-  "userId": number,
-  "id": number,
-  "title": string,
-  "completed": boolean
-}
 
 const handler = async (event: APIGatewayEvent, context: Context) => {
-  const { todo } = event.queryStringParameters;
-  
-  if(!todo) {
-    return {
-      statusCode: 200,
-      body: "todo param needed."
-    }
-  }
-  try {
-    const response: Todo[] =
-      await fetch(`https://jsonplaceholder.typicode.com/todos/${todo}`).then(res => res.json());
+  const { name } = event.queryStringParameters;
 
+  if(!name) {
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        response
-      })
-    };
-  } catch(error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error
-      })
+      body: "name param needed."
     }
   }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      response: `Hello ${name}!`
+    })
+  };
 };
 
 export { handler };
